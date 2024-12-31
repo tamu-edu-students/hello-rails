fe0f776bbf9812047fe13843c33d3c3aded6fa8b
oliviafrancislee <oliviafrancislee@tamu.edu>
2024-12-31 14:44:17 +0800

    fixed seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,11 +9,11 @@ more_movies = [
   {:title => 'Nomadland', :rating => 'R',
     :release_date => '19-Feb-2021'},
   {:title => 'CODA', :rating => 'PG-13',
-    :release_date => '13-Aug-2021'}
+    :release_date => '13-Aug-2021'},
   {:title => 'Spiderman', :rating => 'PG-13',
-    :release_date => '24-Jun-2022'}
+    :release_date => '24-Jun-2022'},
   {:title => 'Spiderman 2', :rating => 'PG-13',
-    :release_date => '31-Dec-2024'}
+    :release_date => '31-Dec-2024'},
   {:title => 'Spiderman 3', :rating => 'PG-13',
   :release_date => '09-Sep-2026'}
 ]
