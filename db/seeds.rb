56ac4eccaceb92b4858335d832b41f6440658c6b
oliviafrancislee <oliviafrancislee@tamu.edu>
2024-12-31 15:11:38 +0800

    initial commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,27 +1,9 @@
-# Seed the RottenPotatoes DB with some movies.
-more_movies = [
-  {:title => 'My Neighbor Totoro', :rating => 'G',
-    :release_date => '16-Apr-1988'},
-  {:title => 'Green Book', :rating => 'PG-13',
-    :release_date => '16-Nov-2018'},
-  {:title => 'Parasite', :rating => 'R',
-    :release_date => '30-May-2019'},
-  {:title => 'Nomadland', :rating => 'R',
-    :release_date => '19-Feb-2021'},
-  {:title => 'CODA', :rating => 'PG-13',
-    :release_date => '13-Aug-2021'},
-  {:title => 'Spiderman', :rating => 'PG-13',
-    :release_date => '24-Jun-2022'},
-  {:title => 'Spiderman 2', :rating => 'PG-13',
-    :release_date => '31-Dec-2024'},
-  {:title => 'Spiderman 3', :rating => 'PG-13',
-  :release_date => '09-Sep-2026'},
-  {:title => 'Spiderman 4', :rating => 'PG-13',
-  :release_date => '10-Jul-2027'},
-  {:title => 'Spiderman 5', :rating => 'PG-13',
-  :release_date => '26-Oct-2027'}
-]
-
-more_movies.each do |movie|
-  Movie.create!(movie)
-end
+# This file should ensure the existence of records required to run the application in every environment (production,
+# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Example:
+#
+#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
+#     MovieGenre.find_or_create_by!(name: genre_name)
+#   end
